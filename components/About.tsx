e3700f5f2e9efda1bb8abda07d00f534b99dbf9c
Lumiho <zavalaleo715@yahoo.com>
2025-11-14 16:47:34 -0800

    Added c/cpp as language known

--- components/About.tsx
+++ components/About.tsx
@@ -45,6 +45,9 @@ export default function About() {
                 Rust
               </span>
               <span className="px-4 py-2 bg-sacramento-100 text-sacramento-700 rounded-full text-sm font-medium">
+                C/C++
+              </span>
+              <span className="px-4 py-2 bg-sacramento-100 text-sacramento-700 rounded-full text-sm font-medium">
                 Machine Learning
               </span>
               <span className="px-4 py-2 bg-sacramento-100 text-sacramento-700 rounded-full text-sm font-medium">
